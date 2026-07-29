de25f5dd0dd7ad4e733298e21b90cec562547793
Emmzain <emmzain222@gmail.com>
2026-07-28 22:00:05 -0700

    feat: embed crisp 512x512 high-resolution icon into native Windows binaries

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -13,6 +13,37 @@ if (-not (Test-Path $binDir)) {
     New-Item -ItemType Directory -Force -Path $binDir | Out-Null
 }
 
+$logoPng = Join-Path $rootDir "assets\logo.png"
+if (Test-Path $logoPng) {
+    Add-Type -AssemblyName System.Drawing
+    $srcBmp = New-Object System.Drawing.Bitmap($logoPng)
+    $ms = New-Object System.IO.MemoryStream
+    $srcBmp.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png)
+    $pngBytes = $ms.ToArray()
+    $ms.Close()
+    $srcBmp.Dispose()
+
+    $bw = New-Object System.IO.BinaryWriter([System.IO.File]::Create($iconPath))
+    $bw.Write([uint16]0) # Reserved
+    $bw.Write([uint16]1) # Type (1 = ICO)
+    $bw.Write([uint16]1) # Count
+
+    # Icon Entry
+    $w = if ($srcBmp.Width -ge 256) { 0 } else { [byte]$srcBmp.Width }
+    $h = if ($srcBmp.Height -ge 256) { 0 } else { [byte]$srcBmp.Height }
+    $bw.Write([byte]0)   # Width (0 = 256)
+    $bw.Write([byte]0)   # Height (0 = 256)
+    $bw.Write([byte]0)   # Colors
+    $bw.Write([byte]0)   # Reserved
+    $bw.Write([uint16]1) # Planes
+    $bw.Write([uint16]32)# BPP
+    $bw.Write([uint32]$pngBytes.Length) # Size
+    $bw.Write([uint32]22) # Offset (6 header + 16 entry = 22)
+
+    $bw.Write($pngBytes)
+    $bw.Close()
+}
+
 if (-not (Test-Path $overlayCs)) {
     throw "Missing overlay\LiteOverlay.cs"
 }
